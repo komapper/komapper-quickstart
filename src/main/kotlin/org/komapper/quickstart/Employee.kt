4d0ab6abbb19af1498fcc9ae6e659d79e6d351f5
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-03-12 12:47:23 +0900

    Use self-mapping (#59)

--- src/main/kotlin/org/komapper/quickstart/Employee.kt
+++ src/main/kotlin/org/komapper/quickstart/Employee.kt
@@ -2,24 +2,21 @@ package org.komapper.quickstart
 
 import org.komapper.annotation.KomapperAutoIncrement
 import org.komapper.annotation.KomapperCreatedAt
-import org.komapper.annotation.KomapperEntityDef
+import org.komapper.annotation.KomapperEntity
 import org.komapper.annotation.KomapperId
 import org.komapper.annotation.KomapperUpdatedAt
 import org.komapper.annotation.KomapperVersion
 import java.time.LocalDateTime
 
+@KomapperEntity
 data class Employee(
+    @KomapperId @KomapperAutoIncrement
     val id: Int = 0,
     val name: String,
+    @KomapperVersion
     val version: Int = 0,
+    @KomapperCreatedAt
     val createdAt: LocalDateTime = LocalDateTime.MIN,
+    @KomapperUpdatedAt
     val updatedAt: LocalDateTime = LocalDateTime.MIN,
-)
-
-@KomapperEntityDef(Employee::class)
-data class EmployeeDef(
-    @KomapperId @KomapperAutoIncrement val id: Nothing,
-    @KomapperVersion val version: Nothing,
-    @KomapperCreatedAt val createdAt: Nothing,
-    @KomapperUpdatedAt val updatedAt: Nothing,
 )
