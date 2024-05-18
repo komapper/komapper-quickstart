eb123ea5b54a0298129749ac048bc129a80d73a9
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2024-05-18 22:59:24 +0900

    Update KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    id("com.google.devtools.ksp") version "1.9.23-1.0.20"
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20"
     kotlin("jvm") version "1.9.24"
 }
 
