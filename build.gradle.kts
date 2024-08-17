b3dedd20bac4f238818dd32a8f4f160a4af264c4
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2024-08-17 10:10:26 +0900

    Update Komapper to v2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ application {
 }
 
 dependencies {
-    val komapperVersion = "2.1.0"
+    val komapperVersion = "2.2.0"
     platform("org.komapper:komapper-platform:$komapperVersion").let {
         implementation(it)
         ksp(it)
