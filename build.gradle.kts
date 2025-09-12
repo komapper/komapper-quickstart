e923d5d37db2073fb1a6112ed609d7641986ad14
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2025-09-13 03:57:17 +0900

    Use JVM 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ tasks {
 
     withType<KotlinCompile>().configureEach {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 }
